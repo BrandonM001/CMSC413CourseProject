39615d17b2e489d6fa28d733035903a4b9afb387
Brandon <mohanbp@vcu.edu>
2021-12-05 16:12:32 -0500

    Fixed login

--- app.py
+++ app.py
@@ -3,18 +3,25 @@ from passwordManager import *
 from passlib.hash import pbkdf2_sha256
 
 app = Flask(__name__)
+username = "_"
 masterPass = "_"
 
 # Route for handling the login page logic
 @app.route('/login', methods=['GET', 'POST'])
 def login():
+    global username
     global masterPass
     error = None
     if request.method == 'POST' and request.form['button'] == 'Login':
-        print(masterPass)
+        username = request.form['username']
         masterPass = request.form['password']
         print(masterPass)
-        num = authenticate(request.form['username'], request.form['password'])
+        print(checkPassRequirements(masterPass) != 1)
+        if(checkPassRequirements(masterPass) != 1):#make sure password is decent
+            error = 'Password does not meet requirements. Please try again.'
+            return render_template('login.html', error=error)
+
+        num = authenticate(username, masterPass)
         print(num)
         #if request.form['username'] != 'admin' or request.form['password'] != 'admin':
         if (num) == -1:
@@ -22,7 +29,14 @@ def login():
         else:
             return redirect(url_for('login'))
     elif request.method == 'POST' and request.form['button'] == 'Register':
-        setupFile(request.form['username'],request.form['password'])
+        username = request.form['username']
+        masterPass = request.form['password']
+        if(checkPassRequirements(masterPass) != 1):#make sure password is decent
+            error = 'Password does not meet requirements. Please try again.'
+            return render_template('login.html', error=error)
+
+        setupFile(username, masterPass)
+        return redirect(url_for('login'))
     return render_template('login.html', error=error)
 
 @app.route('/')
