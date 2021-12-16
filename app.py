769624047facb54d7c8d75ba933c35dfd87dc904
Brandon <mohanbp@vcu.edu>
2021-12-15 22:37:42 -0500

    Added code comments and finished report

--- app.py
+++ app.py
@@ -1,69 +1,71 @@
+#CMSC 413 Course Project Fall 2021
+#By Brandon Mohan and Jared Dun
+
 from flask import Flask, render_template, request, flash, redirect, url_for
 from passwordManager import *
 from passlib.hash import pbkdf2_sha256
 
 app = Flask(__name__)
 username = "_"
-masterPass = "_"
-loggedIn = False
+masterPass = "_"#stores master creds
+loggedIn = False#used to prevent users who aren't logged in from entering site
 
 # Route for handling the login page logic
-@app.route('/login', methods=['GET', 'POST'])
+@app.route('/login', methods=['GET', 'POST'])#login page
 def login():
-    global username
+    global username#use the global variable
     global masterPass
     global loggedIn
     loggedIn = False
     error = None
-    if request.method == 'POST' and request.form['button'] == 'Login':
+    if request.method == 'POST' and request.form['button'] == 'Login':#if they click the login button
         username = request.form['username']
-        masterPass = request.form['password']
+        masterPass = request.form['password']#grab entries
         print(masterPass)
         print(checkPassRequirements(masterPass) != 1)
         if(checkPassRequirements(masterPass) != 1):#make sure password is decent
             error = 'Password does not meet requirements. Please try again.'
             return render_template('login.html', error=error)
 
-        num = authenticate(username, masterPass)
+        num = authenticate(username, masterPass)#check if credentials match
         print(num)
         #if request.form['username'] != 'admin' or request.form['password'] != 'admin':
         if (num) == -1:
-            error = 'Invalid Credentials. Please try again.'
+            error = 'Invalid Credentials. Please try again.' #if credentials don't match
         else:
-            loggedIn = True
+            loggedIn = True#user has logged in, redirect to password page
             return redirect(url_for('manager'))
-    elif request.method == 'POST' and request.form['button'] == 'Register':
+    elif request.method == 'POST' and request.form['button'] == 'Register':#if they click the create account button
         username = request.form['username']
-        masterPass = request.form['password']
+        masterPass = request.form['password']#get credentials from boxes
         if(checkPassRequirements(masterPass) != 1):#make sure password is decent
             error = 'Password does not meet requirements. Please try again.'
             return render_template('login.html', error=error)
-
-        setupFile(username, masterPass)
+        setupFile(username, masterPass)#set up the password file
         return redirect(url_for('login'))
     return render_template('login.html', error=error)
 
-@app.route('/manager', methods=['GET', 'POST'])
+@app.route('/manager', methods=['GET', 'POST'])#passwords are displayed on this page
 def manager():
     global loggedIn
-    if(not loggedIn):
-        return redirect(url_for('login'))
+    if(not loggedIn):#make sure the user is logged in
+        return redirect(url_for('login'))#return to login page
     global masterPass
     error = None
-    if request.method == 'POST' and request.form['button'] == 'Add':
-        if(len(request.form['website'].strip("\n")) > 0 and len(request.form['username'].strip("\n")) > 0 and len(request.form['password'].strip("\n")) > 0):
-            addToFile(request.form['website'],request.form['username'],request.form['password'],masterPass)
+    if request.method == 'POST' and request.form['button'] == 'Add':#if they want to add an entry
+        if(len(request.form['website'].strip("\n")) > 0 and len(request.form['username'].strip("\n")) > 0 and len(request.form['password'].strip("\n")) > 0):#make sure they have all fields
+            addToFile(request.form['website'],request.form['username'],request.form['password'],masterPass)#add credentials to file
     data=[]
-    lines = readAllFromFile(masterPass).split("\n")
+    lines = readAllFromFile(masterPass).split("\n")#grab file contents
     length = len(lines)
-    for i in range(length):
-        if request.method == 'POST' and request.form['button'] == 'DeleteEntry#'+str(i):
+    for i in range(length):#for each entry in file
+        if request.method == 'POST' and request.form['button'] == 'DeleteEntry#'+str(i):#if they click the delete button
             entryToDelete = lines[i].split(",")
-            removeLine(entryToDelete[0],entryToDelete[1])
+            removeLine(entryToDelete[0],entryToDelete[1])#delete the entry
             lines = readAllFromFile(masterPass).split("\n")
             length = len(lines)
     index = 0
-    for line in lines:
+    for line in lines:#put items in webpage
         items = line.split(",")
         i = 0
         dict = {}
@@ -88,7 +90,7 @@ def manager():
 
 @app.route('/')
 def index():
-    return redirect(url_for('login'))
+    return redirect(url_for('login'))#no home page, just login
 
 if __name__ == '__main__':
     app.run(debug=True)
--- passwordManager.py
+++ passwordManager.py
@@ -1,103 +1,98 @@
 #CMSC 413 Password Manager
 #This is the security code
 #Made by Brandon Mohan and Jared Dunnn
+#This file works to transfer data between the file and the webpage
 
 import random
 import re
-from passlib.hash import scrypt
-import cryptocode
+from passlib.hash import scrypt #used to generate hash
+import cryptocode #used to encrypt/decrypt passwords
 
-masterPass = ""
+masterPass = ""#initialize variable
 
+#---------------Function setupFile------------------------
+#This function runs when a new user is created.
+#It works to set up passwords.txt and put the master credentials into hashes in the file
 def setupFile(user, password):
-    #set up passwords.txt and put hashes
-    file = open("passwords.txt", "w")
+    file = open("passwords.txt", "w")#open file
     #assume user/password meets requirements cause Jared checks them
-    salt = random.randint(1000,9999)
-    saltedPass = password + str(salt)
-    hashPass = scrypt.hash(saltedPass)#str(hash(saltedPass))
-    saltedHashPass = hashPass + str(salt)
-    line = str(user) + "|" + str(saltedHashPass) +"\n"
+    salt = random.randint(1000,9999)#add random 4 digit salt
+    saltedPass = password + str(salt)#append salt to password
+    hashPass = scrypt.hash(saltedPass)#hash the pass
+    saltedHashPass = hashPass + str(salt)#add salt to the end of hash
+    line = str(user) + "|" + str(saltedHashPass) +"\n" #syntax to organize file
     masterPass = password
 
-    #print(password)
-    #print(salt)
-    #print(saltedPass)
-    #print(hashPass)
-    #print(saltedHashPass)
+    file.write(line)#write credentials to file
+    file.close()#close file
 
-    file.write(line)
-    file.close()
-
+#---------------Function authenticate-------------------
+#runs when a user logs in via credentials
+#it computes hashes of the entered credentials and compares them to the hashes in the file
 def authenticate(username, password):
-    #grab hashes from file and check
-    file = open("passwords.txt", "r")
-    creds = file.readline().split("|")
-    file.close()
-    grabPass = "".join(creds[1]).strip() #creds[0].strip() #(creds[1])[0:-1]
-    salt = (grabPass)[-4:]
-    saltedPass = password + str(salt)
-    passHash = grabPass[0:-4]
-    #hashPass = str(scrypt.hash(saltedPass))
-    #saltedHashPass = hashPass[2] + str(salt)
-    #print(grabPass)
-    #print(password)
-    #print(salt)
-    #print(saltedPass)
-    #print(hashPass)
-    #print(saltedHashPass)
-    if((creds[0] == username) and (scrypt.verify(saltedPass, passHash))):#grabPass == saltedHashPass)):
+    file = open("passwords.txt", "r")#open and read from text file
+    creds = file.readline().split("|")#parse file input
+    file.close()#close file now that we have what we need
+    grabPass = "".join(creds[1]).strip() #remove file syntax and grab password
+    #creds[0].strip() #(creds[1])[0:-1]
+    salt = (grabPass)[-4:]#get hash salt
+    saltedPass = password + str(salt)#append salt to potential password
+    passHash = grabPass[0:-4]#get stored password hash without appended salt
+    if((creds[0] == username) and (scrypt.verify(saltedPass, passHash))):#check if credentials match
         masterPass = password
         return 1
     return -1
 
+#determines if string meets password requirements
 def checkPassRequirements(password):
     #call when user enters password
-    if(len(password) < 8):
+    if(len(password) < 8):#meet length requirements
         return "length < 8"
     if(not((re.search(".*[0-9].*", password)) and (re.search(".*[a-zA-Z]+.*", password)))):
+        #does password have at least 1 letter and 1 number?
         return "Need # and letter"
-    if(not(hasConsecLetters(password))):
+    if(not(hasConsecLetters(password))):#does it have more than 3 consecutive characters?
         return "No consecutive letters"
 
     return 1
 
-def hasConsecLetters(word):
-    word = word.lower().strip()
-    consecLetters = 0
-    seqLetters = 0
-    preqLetters = 0
-    for i in range(len(word) - 1):
+def hasConsecLetters(word):#checks if string has consecutive characters
+    word = word.lower().strip()#make string as basic as possible
+    consecLetters = 0#checks for consecutive characters ex. 33333 or aaaaa
+    seqLetters = 0  #checks for sequential characters ex. abcde or 12345
+    preqLetters = 0 #checks for sequential characters ex. edcba or 54321
+    for i in range(len(word) - 1):#for each character in a word
         #print(word[i])
-        if(word[i] == word[i+1]):
+        if(word[i] == word[i+1]):#if character is repeated
             consecLetters +=1
-        elif chr(ord(word[i])+1) == word[i+1]:
+        elif chr(ord(word[i])+1) == word[i+1]:#if character increments by 1
             seqLetters +=1
-        elif chr(ord(word[i])-1) == word[i+1]:
+        elif chr(ord(word[i])-1) == word[i+1]:#if character increments by 1
             preqLetters +=1
-        else:
+        else:#if no characters have consecutive issues, return them to 0
             consecLetters = 0
             seqLetters = 0
             preqLetters = 0
-        if(consecLetters == 3 or seqLetters == 3 or preqLetters == 3):
+        if(consecLetters == 3 or seqLetters == 3 or preqLetters == 3):#character has too much repetition
             return 0
 
     return 1
 
-def decryptPass(password, masterPass):
+def decryptPass(password, masterPass):#decrypt the string using the masterpassword
     return cryptocode.decrypt(password, masterPass)
 
-def encryptPass(cipher, masterPass):
+def encryptPass(cipher, masterPass):#encrypt the string using the masterpassword
     return cryptocode.encrypt(cipher, masterPass)
 
-def addToFile(website, username, password, masterPass):
-    file = open("passwords.txt", "a")
-    cipher = encryptPass(password, masterPass)
-    file.write(website + "," + username + "," + cipher + "\n")
-    file.close()
+def addToFile(website, username, password, masterPass):#add given password to file
+    file = open("passwords.txt", "a")#open file
+    cipher = encryptPass(password, masterPass)#encrypt password before adding it
+    file.write(website + "," + username + "," + cipher + "\n")#add file syntax and write
+    file.close()#close file
     return 1
 
-def wipeAllPasswords():
+def wipeAllPasswords():#wipe the file
+#this is a test function, doesn't run in production
     file = open("passwords.txt", "r")
     creds = file.readline()
     file.close()
@@ -105,58 +100,56 @@ def wipeAllPasswords():
     file.write(creds)#doesn't delete passwords, fix later
     file.close()
 
-def removeLine(website, username):
-    print(website)
-    print(username)
-    file = open("passwords.txt", "r")
-    lines = file.readlines()
-    bool = ""
-    fileWrite = open("passwords.txt", "w")
-    fileWrite.write(lines[0])
-    for line in lines[1:]:
+def removeLine(website, username):#removes specific entry from password file
+    #print(website)
+    #print(username)
+    file = open("passwords.txt", "r")#open file
+    lines = file.readlines() #grab data
+    bool = ""#keep track of if we delete an entry
+    file.close()
+    fileWrite = open("passwords.txt", "w")#open file in write mode
+    fileWrite.write(lines[0])#rewrite the master credentials
+    for line in lines[1:]:#for each password entry
         #line = line.strip("\n")
         #print("line: " + line)
-        data = line.split(",")
-        print(data[0])
-        print(data[1])
-        if(data[0] != website or data[1] != username):
+        data = line.split(",") #add file syntax
+        #print(data[0])
+        #print(data[1])
+        if(data[0] != website or data[1] != username):#rewrite to file if not what we're looking for
+            print(line)
             fileWrite.write(line)
         else:
-            bool += line
+            bool += line#we removed a line
 
-    file.close()
-    fileWrite.close()
-    #compareLines = file.readlines()
+    #file.close()
+    fileWrite.close()#close files
     return bool
 
-def ReplaceInFile(username, password):
-    return 0
-
-def readAllFromFile(masterPass):
-    #decrypt things here
-    file = open("passwords.txt", "r")
-    lines = file.readlines()
+def readAllFromFile(masterPass):#grab everything from file and return it
+    file = open("passwords.txt", "r")#open file
+    lines = file.readlines()#grab from file
     entries = ""
     #print(lines)
-    for line in lines[1:]:
+    for line in lines[1:]:#for each line of the file (excluding master creds)
         print("line: " + line)
         line = line.strip()
-        data = line.split(",")
-        data[2] = decryptPass(data[2], masterPass)
-        entries += data[0] + "," + data[1] + "," + str(data[2]) + "\n"
+        data = line.split(",")#remove file syntax
+        data[2] = decryptPass(data[2], masterPass)#decrypt passwords
+        entries += data[0] + "," + data[1] + "," + str(data[2]) + "\n"#prep to send to website
     #print(entries)
+    file.close()#close file
     return entries
 
-def findFromFile(username):
-    file = open("passwords.txt", "r")
+def findFromFile(username):#finds an entry from the file and return it
+    file = open("passwords.txt", "r")#opens the file
     lines = file.readLines()
-    for line in lines:
-        creds = line.split(",")
-        if(creds[1] == username):
+    for line in lines:#for each line of file
+        creds = line.split(",")#split creds
+        if(creds[1] == username):#if we found the right username, return it
             return creds[0]
     return 0
 
-def main():
+def main():#doesn't run in prod, only used for testing
     #setupFile("useer", "passs1256")
     #print("auth")
     #print(authenticate("useer", "passs"))
@@ -170,5 +163,5 @@ def main():
     #print(checkPassRequirements("kek49282d4321"))
 #J sends seach entry of password file
 
-if __name__ == "__main__":
+if __name__ == "__main__":#get main to run if file runs directly
     main()
